4df0fc79dc62e8a48a0d80ea1cf8a97595e90c65
dodipakpahan <dodiarpakpahan@gmail.com>
2021-05-27 11:40:45 +0700

    submit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <br></br>
+        <div className="border">
+          <div className="account">
+            <h1>HI!</h1>
+            <h3>Your Account<br></br>
+            Is Verified</h3>
+          </div>
+          <div className="profil">
+            <img src="/profil.png"></img>
+          </div>
+
+<div className="article">
+<br></br>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has root in a piece of classical</p>
+        <p>Latin literature from 45 BC, making it over 2000 years old. Richard Mc Clintock, a Latin professor at</p>
+        <p>Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur</p>
+        <p>from  a Lorem Ipsum passage, and going throug the cities of the word in classical literature</p>
+        <p>discovered the undoubtable source, Lorem Ipsum comes from sections 1.10.31 and 1.10.33 of "de</p>
+        <p>Finibus Bonorum et Molorum "(The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is</p>
+        <p>a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum</p>
+        <p>"Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32</p><br></br>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
